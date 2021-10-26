168978fd406ca96d5eebc7a8aba4720987587912
wschult23 <wolfgang@schult.biz>
2021-10-26 09:31:31 +0200

    test

---
+++ Analyser/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿// ----------------------------------------------------------------
+// * The LOOM.NET project http://rapier-loom.net *
+// (C) Copyright by Wolfgang Schult. All rights reserved. 
+// This code is licensed under the Apache License 2.0
+// To get more information, go to http://loom.codeplex.com/license
+// ----------------------------------------------------------------
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Loom.SampleWeaver")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Loom.Net Project")]
+[assembly: AssemblyProduct("Loom.SampleWeaver")]
+[assembly: AssemblyCopyright("Copyright © Wolfgang Schult 2006-2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("31cd3e70-7d87-4efb-b548-fb142c82097b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("4.2.1340.0")]
+[assembly: AssemblyFileVersion("4.2.1340.0")]
